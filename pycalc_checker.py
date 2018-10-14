a177171867405ce3a4c5e6851cc088bbdc54dc0d
Aliaksei Buziuma <Aliaksei_Buziuma@epam.com>
2018-10-14 16:05:14 +0300

    update checker

--- pycalc_checker.py
+++ pycalc_checker.py
@@ -52,7 +52,6 @@ COMPARISON_OPERATORS = {
 COMMON_TESTS = {
     "(100)": (100),
     "666": 666,
-    "10(2+1)": 10*(2+1),
     "-.1": -.1,
     "1/3": 1/3,
     "1.0/3.0": 1.0/3.0,
@@ -89,6 +88,16 @@ ERROR_CASES = [
     "pow(2, 3, 4)",
 ]
 
+# OPTIONAL REQUIREMENTS
+IMPLICIT_MULTIPLICATION = {
+    "10(2+1)": 10*(2+1),
+    "(1 + 2)(3 + 4)": (1 + 2) * (3 + 4)
+    "epi": e * pi,
+    "2sin(pi/2)": 2 * sin(pi/2),
+    "2 sin(pi/2)": 2 * sin(pi/2),
+    "sin(pi)sin(pi) + cos(pi)cos(pi)": sin(pi) * sin(pi) + cos(pi) * cos(pi),
+}
+
 
 PASS_TEMPLATE = "Test"
 FAIL_TEMPLATE = ""
@@ -98,15 +107,16 @@ def trunc_string(string):
     return (string[:40] + '..') if len(string) > 40 else string
 
 
-def call_command(command, args):
-    result = subprocess.run([command, "--", args], stdout=subprocess.PIPE)
+def call_command(command, positional_params, optional_params=""):
+    params = [command, "--", positional_params] if optional_params else [command, optional_params, "--", positional_params]
+    result = subprocess.run(params, stdout=subprocess.PIPE)
     return result.stdout.decode('utf-8')
 
 
-def check_results(keys: dict):
+def check_results(keys: dict, required=True, user_module=""):
     global RETURN_CODE
     for command, expected_result in keys.items():
-        result = call_command(PYCALC_UTIL_NAME, command)
+        result = call_command(PYCALC_UTIL_NAME, command, optional_params=user_module)
         try:
             converted_result = float(result)
         except Exception:
@@ -117,7 +127,8 @@ def check_results(keys: dict):
                     command,
                     "{}: Invalid output: {} | Expected: {}".format(colored("FAIL", "red"), result, expected_result))
                 )
-                RETURN_CODE = 1
+                if required:
+                    RETURN_CODE = 1
                 continue
 
         if round(expected_result, 2) == round(converted_result, 2):
@@ -127,10 +138,11 @@ def check_results(keys: dict):
                 command,
                 "{}: Invalid output: {} | Expected: {}".format(colored("FAIL", "red"), result, expected_result))
             )
-            RETURN_CODE = 1
+            if required:
+                RETURN_CODE = 1
 
 
-def check_error_results(keys: list):
+def check_error_results(keys: list, required=True):
     global RETURN_CODE
     for command in keys:
         result = call_command(PYCALC_UTIL_NAME, command)
@@ -139,10 +151,12 @@ def check_error_results(keys: list):
         else:
             print("{: <45} | Result: {}".format(trunc_string(command),
                                                 "{}, expected correct error handling".format(colored("FAIL", "red"))))
-            RETURN_CODE = 1
+            if required:
+                RETURN_CODE = 1
 
 
 def main():
+    print(colored("************** Checking minimal requirements **************\n", "green"))
     print("Checking unary operators...")
     check_results(UNARY_OPERATORS)
     print()
@@ -169,6 +183,12 @@ def main():
 
     print("Checking error cases...")
     check_error_results(ERROR_CASES)
+    print()
+
+    print(colored("************** Checking optional requirements **************\n", "green"))
+    check_results(IMPLICIT_MULTIPLICATION, required=False)
+    print()
+
 
     sys.exit(RETURN_CODE)
 
